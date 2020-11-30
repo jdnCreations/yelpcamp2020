b6d5aa3e710bc2be4e7b27b1a996033b0f88f135
Jordan Needham <jdnCreations@hotmail.com>
2020-11-30 10:58:10 +0800

    Added controllers and split routes into individual files

---
+++ controllers/users.js
@@ -0,0 +1,39 @@
+const User = require('../models/user');
+const passport = require('passport');
+
+module.exports.renderRegisterForm = (req, res) => {
+    res.render('users/register');
+};
+
+module.exports.newUser = async (req, res) => {
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            req.flash('success', `Welcome to YelpCamp, ${username}!`);
+            res.redirect('/campgrounds');
+        });
+    } catch (e) {
+        req.flash('error', e.message);
+        res.redirect('/register');
+    }
+};
+
+module.exports.renderLoginForm = (req, res) => {
+    res.render('users/login');
+};
+
+module.exports.loginUser = (req, res) => {
+    req.flash('success', 'Welcome back!');
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
+};
+
+module.exports.logoutUser = (req, res) => {
+    req.logout();
+    req.flash('success', 'You have logged out.');
+    res.redirect('/campgrounds');
+};
